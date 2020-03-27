dc71a630ba2e9b4a08d26a3b1b4a29e36a3f72b9
rilwan11 <rilwan11@gmail.com>
2020-03-27 06:16:42 -0700

    Update scrapper.py

--- src/scrapper.py
+++ src/scrapper.py
@@ -1,12 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+
 def web():
-    url = input ("Enter url to scrap:") 
+    #collect user input 
+    url = input ("Enter url to scrap: ") 
     print(url) 
+    
+    #get the text of the user's input
     code = requests.get(url)
     plain = code.text
+    
+    #convert the user's input into html
     s = BeautifulSoup(plain, "html.parser")
     print(s)
+    
+    #loop through the page dom elements that we need 
     # for link in s.findAll('a', {'class':'s-access-detail-page'}):
     #     tet = link.get('title')
     #     print(tet)
